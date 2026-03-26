17b38aec9a751a39e2b12bb79d11677ca42bf641
AdamMeddah <78007082+AdamMeddah@users.noreply.github.com>
2026-03-26 02:43:26 -0400

    DIALOGUE: mark intro & account, iris intro and account, kevin intro

--- systems/game_controller.gd
+++ systems/game_controller.gd
@@ -101,7 +101,8 @@ func _on_door_entered(door: Door):
 	if door.to_scene == null:
 		return
 	
-	change_scene(door.to_scene, door.to_door_id)
+	start_minigame()
+	#change_scene(door.to_scene, door.to_door_id)
 
 
 func _on_dialogue_start():
